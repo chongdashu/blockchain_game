a6a4f4bcbb5f1ba15cf85719d439a9d3e1ab9692
Chong-U Lim <me@chongdashu.com>
2021-10-16 02:40:05 +0100

    implement mint and add tests - 44:14

--- test/MemoryToken.test.js
+++ test/MemoryToken.test.js
@@ -32,6 +32,41 @@ contract('Memory Token', (accounts) => {
       const symbol = await token.symbol()
       assert.equal(symbol, 'MEMORY')
     })
-
   })
+
+  describe("token distribution", async() => {
+    let result
+
+    it('mints tokens', async () => {
+      await token.mint(accounts[0], 'https://www.token-uri.com/nft')
+      
+      // it should increase total supply
+      result = await token.totalSupply() 
+      assert.equal(result.toString(), '1', 'total supply is correct')
+
+      // it increments owner balance
+      result = await token.balanceOf(accounts[0])
+      assert.equal(result.toString(), '1', 'balanceOf is correct')
+
+      // token should belong to owner
+      result = await token.ownerOf('1')
+      assert.equal(result.toString(), accounts[0].toString(), 'ownerOf is correct')
+      result = await token.tokenOfOwnerByIndex(accounts[0], 0)
+
+      // owner can see all tokens
+      let balanceOf = await token.balanceOf(accounts[0])
+      let tokenIds = []
+      for (let i=0; i < balanceOf; i++) {
+        let id = await token.tokenOfOwnerByIndex(accounts[0], i)
+        tokenIds.push(id.toString())
+      }
+      let expected = ['1']
+      assert.equal(tokenIds.toString(), expected.toString(), 'tokenIds are correct')
+
+      // token uri is correct
+      let tokenURI = await token.tokenURI('1')
+      assert.equal(tokenURI, 'https://www.token-uri.com/nft')
+    })
+  })
+  
 })
